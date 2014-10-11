ecc7ff6dfdbb1942bda3c4130608c64e29e16f18
FlicAnderson <contact@felicityanderson.co.uk>
2014-10-11 14:33:22 +0100

    code for creating plot4.png

---
+++ plot4.R
@@ -0,0 +1,10 @@
+## Coursera :: ExploratoryDataAnalysis :: CourseProject1 :: plot4.R
+## ======================================================================== 
+## (11th October 2014)
+## Author: Flic Anderson
+##
+
+## AIM:  create plot4.png
+# ... 
+
+###
