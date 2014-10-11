733026032dc2092b6372c250a82f16378a753eec
FlicAnderson <contact@felicityanderson.co.uk>
2014-10-11 14:33:15 +0100

    code for creating plot3.png

---
+++ plot3.R
@@ -0,0 +1,10 @@
+## Coursera :: ExploratoryDataAnalysis :: CourseProject1 :: plot3.R
+## ======================================================================== 
+## (11th October 2014)
+## Author: Flic Anderson
+##
+
+## AIM:  create plot3.png
+# ... 
+
+###
