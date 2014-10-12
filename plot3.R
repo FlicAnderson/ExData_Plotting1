eb59cb9a2f1a55e1bd29850499a4037a40e6ebe9
FlicAnderson <contact@felicityanderson.co.uk>
2014-10-12 13:44:09 +0100

    scipt to create plot3.png figure from sourced data of dataOperations.R

--- plot3.R
+++ plot3.R
@@ -5,6 +5,34 @@
 ##
 
 ## AIM:  create plot3.png
-# ... 
 
-###
+# load and subset the data
+source("dataOperations.R")
+
+# set up graphics device for plot
+png(file="plot3.png")
+# plot Sub_metering_1
+plot(
+  x=datA$DateTime, 
+  xlab="",
+  y=datA$Sub_metering_1, 
+  ylab="Energy sub metering",
+  type="l"
+)
+# add Sub_metering_2
+lines(
+  x=datA$DateTime, 
+  y=datA$Sub_metering_2, 
+  type="l",
+  col="red")
+# add Sub_metering_3
+lines(x=datA$DateTime, 
+  y=datA$Sub_metering_3, 
+  type="l",
+  col="blue")
+# add legend
+legend("topright", c(names(datA[6:8])), col=c("black", "red", "blue"), lty="solid")
+# write plot to graphics device
+dev.off()
+
+
