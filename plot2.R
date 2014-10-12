8173bbebbc224f54e3132c6653ae585c7fe65771
FlicAnderson <contact@felicityanderson.co.uk>
2014-10-12 13:05:23 +0100

    script to create plot2.png

--- plot2.R
+++ plot2.R
@@ -5,6 +5,19 @@
 ##
 
 ## AIM:  create plot2.png
-# ... 
 
-###
+# load and subset the data
+source("dataOperations.R")
+
+# set up graphics device for plot
+png(file="plot2.png")
+# plot 
+plot(
+  x=datA$DateTime, 
+  xlab="",
+  y=datA$Global_active_power, 
+  ylab="Global Active Power (kilowatts)",
+  type="l"
+  )
+# write plot to graphics device
+dev.off()
